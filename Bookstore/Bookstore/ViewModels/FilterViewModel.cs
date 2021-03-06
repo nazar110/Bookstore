40eb5de267bf8e76dc9d1683107eb088327a60ff
nazar110 <nazar1124@gmail.com>
2021-03-06 14:32:40 +0200

    add home page functionality

---
+++ Bookstore/Bookstore/ViewModels/FilterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.ViewModels
+{
+    public class FilterViewModel
+    {
+        public string Filter { get; set; } = null;
+
+        public FilterViewModel(string filterBy)
+        {
+            Filter = filterBy;
+        }
+    }
+}
---
+++ Bookstore/Bookstore/ViewModels/SortViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Bookstore.Helpers;
+
+namespace Bookstore.ViewModels
+{
+    public class SortViewModel
+    {
+        public SortState TitleSort { get; private set; }
+        public SortState AuthorNameSort { get; private set; }
+        public SortState GenreSort { get; private set; }
+        public SortState PriceSort { get; private set; }
+        public SortState Current { get; private set; }
+
+        public SortViewModel(SortState sortOrder)
+        {
+            TitleSort = sortOrder == SortState.TitleAsc ? SortState.TitleDesc : SortState.TitleAsc;
+            AuthorNameSort = sortOrder == SortState.AuthorNameAsc ? SortState.AuthorNameDesc : SortState.AuthorNameAsc;
+            GenreSort = sortOrder == SortState.GenreAsc ? SortState.GenreDesc : SortState.GenreAsc;
+            PriceSort = sortOrder == SortState.PriceAsc ? SortState.PriceDesc : SortState.PriceAsc;
+            Current = sortOrder;
+        }
+    }
+}
